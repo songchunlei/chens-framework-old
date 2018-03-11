dfbd3a2e7e3311f8cbc3ccf9a10f75f73daaa34d
songchunlei <chunlei.song@live.com>
2018-03-11 21:44:22 +0800

    源码来自ueditor-1.4.33，梳理了目录结构，待测试

---
+++ chens-ueditor/src/main/java/com/chens/ueditor/UeditorConfiguration.java
@@ -0,0 +1,15 @@
+package com.chens.ueditor;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置
+ *
+ * @auther songchunlei@qq.com
+ * @create 2018/3/11
+ */
+@Configuration
+@ComponentScan
+public class UeditorConfiguration {
+}
---
+++ chens-ueditor/src/main/java/com/chens/ueditor/controller/UeditorController.java
@@ -0,0 +1,10 @@
+package com.chens.ueditor.controller;
+
+/**
+ * 通用服务
+ *
+ * @auther songchunlei@qq.com
+ * @create 2018/3/11
+ */
+public class UeditorController {
+}
