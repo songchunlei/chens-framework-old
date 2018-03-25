10d3a71e01b846ffe13d6f90c32d6ff0ad116d42
songchunlei <chunlei.song@live.com>
2018-03-25 13:46:27 +0800

    增加mybatis配置

---
+++ chens-auth/chens-auth-server/src/main/java/com/chens/auth/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.chens.auth.config;
+
+import com.baomidou.mybatisplus.spring.boot.starter.MybatisPlusProperties;
+import com.chens.core.config.BaseMybatisPlusConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Mybatis-plus配置
+ *
+ * @auther songchunlei@qq.com
+ * @create 2018/2/12
+ */
+@Configuration
+@EnableConfigurationProperties(MybatisPlusProperties.class)
+@MapperScan("com.chens.auth.mapper*")
+public class MybatisPlusConfig extends BaseMybatisPlusConfig{
+
+
+}
