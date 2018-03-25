2848ee57375a3f80b6733751921cd92bf8cdf5b7
songchunlei <chunlei.song@live.com>
2018-03-25 14:29:36 +0800

    租户id

--- chens-core/src/main/java/com/chens/core/config/BaseMybatisPlusConfig.java
+++ chens-core/src/main/java/com/chens/core/config/BaseMybatisPlusConfig.java
@@ -46,13 +46,12 @@ public class BaseMybatisPlusConfig {
      * 分页插件
      * @return
      */
+    /*
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         PaginationInterceptor page = new PaginationInterceptor();
-        /*
-         * 【测试多租户】 SQL 解析处理拦截器<br>
-         * 这里固定写成住户 1 实际情况你可以从cookie读取，因此数据看不到 【 麻花藤 】 这条记录（ 注意观察 SQL ）<br>
-         */
+        //测试多租户】 SQL 解析处理拦截器<br>
+        //这里固定写成住户 1 实际情况你可以从cookie读取，因此数据看不到 【 麻花藤 】 这条记录（ 注意观察 SQL ）<br>
         List<ISqlParser> sqlParserList = new ArrayList<>();
         TenantSqlParser tenantSqlParser = new TenantSqlParser();
         tenantSqlParser.setTenantHandler(new TenantHandler() {
@@ -88,11 +87,10 @@ public class BaseMybatisPlusConfig {
         sqlParserList.add(tenantSqlParser);
         page.setSqlParserList(sqlParserList);
 
-        /**
-         * 可以用 @SqlParser(filter = true) 注解取消某个mapper下方法的租户信息
-         */
+        //可以用 @SqlParser(filter = true) 注解取消某个mapper下方法的租户信息
         return page;
     }
+    */
 
     /**
      * 注入公共字段自动填充,任选注入方式即可
