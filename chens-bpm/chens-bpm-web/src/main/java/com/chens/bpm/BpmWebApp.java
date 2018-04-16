8386db9a81158c6d998bd57cbbce11cbc975490d
songchunlei <chunlei.song@live.com>
2018-04-16 23:48:32 +0800

    流程服务

--- chens-bpm/chens-bpm-web/src/main/java/com/chens/bpm/BpmWebApp.java
+++ chens-bpm/chens-bpm-web/src/main/java/com/chens/bpm/BpmWebApp.java
@@ -1,10 +1,26 @@
 package com.chens.bpm;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 /**
  * 启动
  *
  * @author songchunlei@qq.com
  * @create 2018/4/15
  */
+@SpringBootApplication
+@ComponentScan("com.chens")
+@EnableTransactionManagement
+@EnableEurekaClient
+@EnableFeignClients(basePackages = "com.chens")
 public class BpmWebApp {
+    public static void main(String[] args){
+        new SpringApplicationBuilder(BpmWebApp.class).web(true).run(args);
+
+    }
 }
